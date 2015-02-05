3d661a5dcb800abf95db0219442ebdc4847ff546
wyicwx <wyicwx@gmail.com>
2015-02-05 16:52:25 +0800

    update

--- bonefile.js
+++ bonefile.js
@@ -1,65 +1,46 @@
 var bone = require('bone');
 var path = require('path');
 var fs = require('fs');
-
-// load sub-project bonefile 
-function requireBonefile(folders) {
-	folders.forEach(function(folder) {
-		var subbonefile = path.join(folder, 'subbonefile.js');
-		if(fs.existsSync(subbonefile)) {
-			require(subbonefile);
-		}
-	});
-}
-var projectsPath = path.resolve('./projects');
-var projects = fs.readdirSync(projectsPath).map(function(v) {
-	return path.join(projectsPath, v);
-});
-var commonsPath = path.resolve('./common');
-var commons = fs.readdirSync(commonsPath).map(function(v) {
-	return path.join(commonsPath, v);
-});
-requireBonefile(projects);
-requireBonefile(commons);
-
-// map ./common to ./project/sub-project/common
-projects.forEach(function(project) {
-	bone.dest(project)
-		.dest('common')
-		.src(path.join(commonsPath, '**/*'));
-});
-
-
-
 var connect = require('bone-connect');
 var less = require('bone-less');
 var include = require('bone-act-include');
 
+// common
+var common = bone.dest('common');
+common.dest('css3')
+	.src('./animate.less')
+	.act(include)
+	.act(less)
+	.rename('animate.css');
 
 
-var dist = bone.dest('dist');
 
-// var turn = dist.dest('turn');
 
-// turn.src('~/projects/turn/src/**/*');
+// load project 
+fs.readdirSync('./projects').map(function(v) {
+	var p = path.join(__dirname, 'projects', v, 'bone.js');
+	p = path.normalize(p);
 
-// turn.src('~/projects/turn/*.html');
+	if(fs.existsSync(p)) {
+		require(p);
+	}
+});
 
-// dist.dest('helper')
-// 	.src('~/projects/helper/*');
+bone.dest('dist')
+	.src('~/projects/**/*');
 
-// var css = dist.dest('css');
+bone.project('dist', '~/dist/**/*');
 
-// css.src('~/projects/css3/*.less')
-// 	.act(include())
-// 	.act(less())
-// 	.rename(function(filename) {
-// 		return filename.replace(/\.less$/, '.css');
-// 	});
-
+bone.task('release', function() {
+	bone.fs.rm('~/dist');
+},{
+	name: 'build',
+	params: {
+		'project': 'dist'
+	}
+});
+// cli
 bone.cli(connect({
 	base: './',
 	port: 8081
 }));
-
-bone.project('dist', '~/projects/**/*');
---
+++ projects/turn/bone.js
@@ -0,0 +1,1 @@
+var bone = require('bone');
